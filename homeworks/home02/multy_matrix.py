589b5d4080a28de05023d808babb03c5d43a8c99
Alex Klyanchin <k142@mail.ru>
2021-07-09 00:17:16 +0300

    home02 beta1

--- homeworks/home02/multy_matrix.py
+++ homeworks/home02/multy_matrix.py
@@ -8,19 +8,22 @@ def matrix_multi(A, B):
     A_n = len(A[0])
     B_n = len(B)
     B_k = len(B[0])
-    C = [[None]*A_m]*B_k
+    C = [[[] for i in range(A_m)] for i in range(B_k)]
 
     for i in range(A_m):
         for j in range(B_k):
             sum0 = 0
             for p in range(A_n):
                 sum0 += A[i][p] * B[p][j]
-            # print(i, j, sum0)
-            print(C)
             C[i][j] = sum0
     return C
 
 
-a = [[1, 2, 3], [2, 3, 4]]
-b = [[2, 1], [3, 1], [1, 1]]
-print(matrix_multi(a, b))
+A = [[1, 2, 3], [2, 3, 4]]
+B = [[2, 1], [3, 1], [1, 1]]
+print(matrix_multi(A, B))
+
+A = [[1, -2], [3, 0]]
+B = [[4, -1], [0, 5]]
+print(matrix_multi(A, B))
+print(matrix_multi(B, A))
